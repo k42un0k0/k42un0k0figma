9f7f30d9226e37fc2a8f11efc097bedf3c74be2b
かずのこ <k42un0k0.put1put1@gmail.com>
2021-09-17 19:08:01 +0900

    react-pixi-fiberを利用する

--- lib/canvas/utils.ts
+++
@@ -1,9 +0,0 @@
-export function onGuard<T extends keyof HTMLElementEventMap>(
-  typeActual: T,
-  tuple: [
-    type: keyof HTMLElementEventMap,
-    e: HTMLElementEventMap[keyof HTMLElementEventMap]
-  ]
-): tuple is [type: T, e: HTMLElementEventMap[T]] {
-  return typeActual === tuple[0];
-}
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,20 +1,7 @@
-import Head from "next/head";
-import Image from "next/image";
-import { useEffect, useRef } from "react";
-import { Rect } from "../lib/canvas/entity";
-import { Scene } from "../lib/canvas/root";
-import styles from "../styles/Home.module.css";
+import dynamic from "next/dynamic";
+
+const Unko = dynamic(() => import("../components/index"));
 
 export default function Home() {
-  const ref = useRef<HTMLCanvasElement>(null);
-  useEffect(() => {
-    if (typeof window == "undefined") return;
-    if (ref.current == null) return;
-    const scene = new Scene(ref.current);
-    scene.append(new Rect(scene, 0, 0, 100, 100, 20));
-    scene.append(new Rect(scene, 100, 100, 100, 100, 0));
-    scene.append(new Rect(scene, 200,200, 100, 100, 0));
-    scene.draw();
-  });
-  return <canvas ref={ref}  width="500" height="500"/>;
+  return <Unko />;
 }
